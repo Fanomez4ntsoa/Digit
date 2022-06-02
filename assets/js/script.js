745e8b48b4c6dab70aef4af66668f14230c74471
Fanomez4ntsoa <voary.randrianarison@esti.mg>
2022-06-02 09:01:01 +0300

    Update css & Configuration navbar

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+//execute script after page load
+window.onload = function digital_fn() {
+
+	//toggle button
+	let toggle = document.querySelector("#nav .toggle-btn")
+	let collapse = document.querySelector("#nav .collapse")
+
+	toggle.addEventListener('click', function(event) {
+		// console.log(toggle)
+		collapse.classList.toggle('active')
+	});
+}
